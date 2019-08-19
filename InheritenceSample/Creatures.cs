29b00ff5707d15691ea4f0bd81d4a67587799add
Tanner Disney <tanner_disney@yahoo.com>
2019-08-19 13:21:33 -0700

    Add project files.

---
+++ InheritenceSample/Creatures.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritenceSample
+{
+    /// <summary>
+    /// A base class to represent human and AI controlled characters
+    /// </summary>
+    class Creature
+    {
+
+        public Creature(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// The name of a creature
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The health points of a creature
+        /// </summary>
+        public double Health { get; set; }
+
+        /// <summary>
+        /// The attack points of a creature
+        /// </summary>
+        public int Attack { get; set; }
+
+        /// <summary>
+        /// The defence points of a creature
+        /// </summary>
+        public int Defence { get; set; }
+
+        /// <summary>
+        /// The speed points of a creature
+        /// </summary>
+        public int Speed { get; set; }
+
+        /// <summary>
+        /// The creature's race -
+        /// example: Human, Half-Orc, Demon
+        /// </summary>
+        public string Race { get; set; }
+
+        public virtual string GetDisplayText(string sep)
+        {
+            return $"{Name}{sep}{Health}{sep}{Race}";
+        }
+    }
+
+    /// <summary>
+    /// A Human Class to repesent a Non-AI Controlled character
+    /// </summary>
+    class Player : Creature
+    {
+        public Player(string playerName) : base(playerName)
+        {  }
+
+        /// <summary>
+        /// Stamina is used when running or performing physical action's
+        /// </summary>
+        public double Stamina { get; set; }
+        /// <summary>
+        /// These are magic points to be spent on casting spells
+        /// </summary>
+        public double Mana { get; set; }
+
+        public override string GetDisplayText(string sep)
+        {
+            return "Player: " + base.GetDisplayText(sep) + sep + Stamina;
+        }
+    }
+
+    /// <summary>
+    /// An AI Class to represent an AI-Controlled character
+    /// </summary>
+    [DebuggerDisplay("EnemyId: {EnemyId}")]
+    class Enemy : Creature
+    {
+        public Enemy(string enemyName) : base(enemyName)
+        { }
+
+        /// <summary>
+        /// The Id of the enemy
+        /// </summary>
+        public int EnemyId { get; set; }
+
+        /// <summary>
+        /// How long the enemy takes to repsawn after they are defeated.
+        /// If the enemy does not have a RespawnRate, once they are defeated they never respawn.
+        /// </summary>
+        public TimeSpan? RespawnRate { get; set; }
+
+        /// <summary>
+        /// The number of experience points a player
+        /// gains on deafeating an enemy.
+        /// </summary>
+        public int ExperienceDropRate { get; set; }
+
+        /// <summary>
+        /// Items the enemy may be able to drop on being defeated
+        /// </summary>
+        public List<Item> ItemDrops { get; set; }
+    }
+}
---
+++ InheritenceSample/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritenceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player p1 = new Player("Link");
+            p1.Attack = 100;
+            p1.Health = 25.5;
+            p1.Stamina = 15.5;
+
+            Item potion = new Item()
+            {
+                ItemId = 1000,
+                Name = "Small Healing Potion",
+                MonetaryValue = 25
+            };
+
+            Item sword = new Item()
+            {
+                ItemId = 5000,
+                Name = "Greatsword",
+                MonetaryValue = 1000
+            };
+
+
+            Enemy e1 = new Enemy("Ganon");
+            e1.EnemyId = 1;
+            e1.Defence = 100;
+            // 5.5 hours to respawn
+            e1.RespawnRate = new TimeSpan(5, 30, 0);
+            e1.ItemDrops.Add(potion);
+            e1.ItemDrops.Add(sword);
+
+            Creature c = new Player("Zelda");
+            // How can we set stamina when we know the creature is a player
+            //c.Stamina = 5;
+
+            DisplayCreature(p1);
+            DisplayCreature(e1);
+            DisplayCreature(c);
+
+            Console.ReadKey();
+            
+        }
+
+        private static void DisplayCreature(Creature c)
+        {
+            if (c is Enemy)
+            {
+                Enemy currEnemy = c as Enemy;
+                Console.WriteLine(currEnemy.EnemyId);
+                Console.WriteLine("Hidden Stats...");
+            }
+            else
+            {
+                Console.WriteLine(c.GetDisplayText("\n"));
+            }
+        }
+    }
+}
